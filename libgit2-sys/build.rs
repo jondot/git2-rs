1cde9568a17c069e85155a9d35ac49ede6a77921
Dotan J. Nahum <jondotan@gmail.com>
2021-03-07 17:57:52 +0200

    Update build.rs

--- libgit2-sys/build.rs
+++ libgit2-sys/build.rs
@@ -179,7 +179,7 @@ fn main() {
     }
 
     if target.contains("apple") {
-        println!("cargo:rustc-link-lib=iconv");
+        println!("cargo:rustc-link-lib=static=iconv");
         println!("cargo:rustc-link-lib=framework=Security");
         println!("cargo:rustc-link-lib=framework=CoreFoundation");
     }
